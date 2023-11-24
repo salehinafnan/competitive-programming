bda9c52fa82a4a58b7d2c0abc30a35dbff12ddbe
salehinafnan <salehinafnan@gmail.com>
2023-11-24 13:08:07 +0600

    latest

---
+++ Cricket_World_Cup_Qualifier.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x;
+    cin >> x;
+    if (x >= 12)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
